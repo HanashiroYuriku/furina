6f6713d6d951156786fc1719da5ab40690b7b036
HanashiroYuriku <devan051003@gmail.com>
2026-04-18 01:02:32 +0700

    add: add struct user, user response, and user request

---
+++ internal/core/entity/user.go
@@ -0,0 +1,36 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// User represents a user in the system.
+type User struct {
+	ID         string `gorm:"primaryKey;type:varchar(50)"`
+	Username   string `gorm:"unique;not null;type:varchar(20)"`
+	Email      string `gorm:"unique;not null;type:varchar(100)"`
+	Password   string `gorm:"not null;type:varchar(255)"`
+	Role       string `gorm:"type:varchar(20);default:'user';check:role IN ('admin', 'user')"`
+	IsVerified bool   `gorm:"default:false"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+}
+
+// user struct for api response
+type UserResponse struct {
+	ID         string `json:"id"`
+	Username   string `json:"username"`
+	Email      string `json:"email"`
+	Role       string `json:"role"`
+	IsVerified bool   `json:"isVerified"`
+}
+
+// user struct for api request
+type UserRequest struct {
+	Username string `json:"username" validate:"required,min=3,max=20,unique=user->username,whitespace,username"`
+	Email    string `json:"email" validate:"required,max=100,email,unique=user->email,whitespace"`
+	Password string `json:"password" validate:"required,complexpassword"`
+}
