67f8a92cc481c4eafe6412b8d303771d71e0a2e8
HanashiroYuriku <devan051003@gmail.com>
2026-04-18 00:34:50 +0700

    fix: change file name add function to verify user email

--- internal/adapter/repository/user_repo.go
+++ internal/adapter/repository/user_repo.go
@@ -1,8 +1,9 @@
 package repository
 
 import (
+	"be-ayaka/internal/core/customerrors"
 	"be-ayaka/internal/core/entity"
-	"be-ayaka/internal/core/repository"
+	"be-ayaka/internal/core/port"
 	"errors"
 
 	"gorm.io/gorm"
@@ -12,7 +13,7 @@ type userRepoPostgres struct {
 	db *gorm.DB
 }
 
-func NewUserRepoPostgres(db *gorm.DB) repository.UserRepository {
+func NewUserRepoPostgres(db *gorm.DB) port.UserRepository {
 	return &userRepoPostgres{
 		db: db,
 	}
@@ -23,15 +24,15 @@ func (r *userRepoPostgres) FindByID(id string) (*entity.User, error) {
 	err := r.db.First(&user, "id = ?", id).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
+			return nil, customerrors.ErrDataNotFound
 		}
 		return nil, err
 	}
 	return &user, nil
 }
 
-func (r *userRepoPostgres) FindByEmailUsername(value string) (*entity.User, error) {
-	var user entity.User
+func (r *userRepoPostgres) FindByEmailUsername(value string) (*entity.UserResponse, error) {
+	var user entity.UserResponse
 	err := r.db.Where("email = ? OR username = ?", value, value).First(&user).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
@@ -44,4 +45,8 @@ func (r *userRepoPostgres) FindByEmailUsername(value string) (*entity.User, erro
 
 func (r *userRepoPostgres) Create(user *entity.User) error {
 	return r.db.Create(user).Error
+}
+
+func (r *userRepoPostgres) VerifUser(id string) error {
+	return r.db.Model(&entity.User{}).Where("id = ?", id).Update("is_verified", true).Error
 }
