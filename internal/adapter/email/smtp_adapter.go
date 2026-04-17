28864e482718991be63f28ac0be7823bcdbfc177
HanashiroYuriku <devan051003@gmail.com>
2026-04-18 00:33:59 +0700

    fix: tidy up code

--- internal/adapter/email/smtp_adapter.go
+++ internal/adapter/email/smtp_adapter.go
@@ -7,17 +7,17 @@ import (
 )
 
 type smtpAdapter struct {
-	host string
-	port string
-	user string
+	host     string
+	port     string
+	user     string
 	password string
 }
 
 func NewSMTPAdapter(host, port, user, password string) port.EmailSender {
 	return &smtpAdapter{
-		host: host,
-		port: port,
-		user: user,
+		host:     host,
+		port:     port,
+		user:     user,
 		password: password,
 	}
 }
@@ -26,7 +26,7 @@ func (s *smtpAdapter) SendEmail(to, subject, htmlBody string) error {
 	auth := smtp.PlainAuth("", s.user, s.password, s.host)
 	address := fmt.Sprintf("%s:%s", s.host, s.port)
 
-	msg := fmt.Sprintf("MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\nFrom: %s\nSubject: %s\n\n%s",
+	msg := fmt.Sprintf("MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\nFrom: %s\nTo: %s\nSubject: %s\n\n%s",
 		s.user, to, subject, htmlBody)
 
 	err := smtp.SendMail(address, auth, s.user, []string{to}, []byte(msg))
