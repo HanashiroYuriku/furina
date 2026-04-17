96007148564e06a3b05eac19ccfd1726974797ca
HanashiroYuriku <devan051003@gmail.com>
2026-04-18 01:22:43 +0700

    add: add global error handler

---
+++ internal/middleware/error_handler.go
@@ -0,0 +1,76 @@
+package middleware
+
+import (
+	"be-ayaka/internal/core/customerrors"
+	"be-ayaka/pkg/response"
+	"be-ayaka/pkg/utils"
+	"errors"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GlobalErrorHandler(c *fiber.Ctx, err error) error {
+	requestId := utils.GetRequestID(c)
+
+	var valErr *customerrors.ValidationError
+
+    if errors.As(err, &valErr) {
+        return c.Status(fiber.StatusUnprocessableEntity).JSON(
+            response.NewErrorFieldResponse(
+                response.UnprocessableEntity,
+                valErr.Error(),
+                valErr.Detail,
+                requestId,
+            ),
+        )
+    }
+
+	code := fiber.StatusInternalServerError
+	statusString := response.InternalServerError
+	message := "Internal Server Error"
+
+	var fiberErr *fiber.Error
+	if errors.As(err, &fiberErr) {
+		code = fiberErr.Code
+		message = fiberErr.Message
+	}
+
+	switch {
+	case errors.Is(err, customerrors.ErrDataNotFound):
+		code = fiber.StatusNotFound
+		statusString = response.DataNotFound
+		message = err.Error()
+
+	case errors.Is(err, customerrors.ErrInvalidPassword),
+		errors.Is(err, customerrors.ErrTokenExpired):
+		code = fiber.StatusUnauthorized
+		statusString = response.Unauthorized
+		message = err.Error()
+
+	case errors.Is(err, customerrors.ErrDataConflict):
+		code = fiber.StatusConflict
+		statusString = response.DataConflict
+		message = err.Error()
+
+	case errors.Is(err, customerrors.ErrBadRequest):
+		code = fiber.StatusBadRequest
+		statusString = response.BadRequest
+		message = err.Error()
+
+	case errors.Is(err, customerrors.ErrColldownActive):
+		code = fiber.StatusTooManyRequests
+		statusString = response.TooManyRequests
+		message = err.Error()
+
+	case errors.Is(err, customerrors.ErrTokenExpired):
+		code = fiber.StatusUnauthorized
+		statusString = response.Unauthorized
+		message = err.Error()
+	}
+
+	return c.Status(code).JSON(response.NewErrorResponse(
+		statusString,
+		message,
+		requestId,
+	))
+}
