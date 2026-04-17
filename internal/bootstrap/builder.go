3fbaafff8f3671e7bf2d381c1c1f7e7d717c5c70
HanashiroYuriku <devan051003@gmail.com>
2026-04-18 00:37:13 +0700

    fix: add all inject to one function; add auth inject

--- internal/bootstrap/builder.go
+++ internal/bootstrap/builder.go
@@ -1,6 +1,8 @@
 package bootstrap
 
 import (
+	"be-ayaka/config"
+	"be-ayaka/internal/adapter/email"
 	adapterRepo "be-ayaka/internal/adapter/repository"
 	"be-ayaka/internal/core/service"
 	"be-ayaka/internal/delivery/http"
@@ -10,13 +12,35 @@ import (
 	"gorm.io/gorm"
 )
 
-func BuildUserHandler(db *gorm.DB) *http.UserHandler {
-	// adapter
-	userRepo := adapterRepo.NewUserRepoPostgres(db)
-	// service
-	hashService := hash.NewBcryptHash()
-	userService := service.NewUserService(userRepo, hashService)
-	// handler
+type Handlers struct {
+	Auth *http.AuthHandler
+}
+
+func BuildAllDependencies(db *gorm.DB, cfg *config.Config) *Handlers {
+	// === email adapter
+	emailAdapter := email.NewSMTPAdapter(
+		cfg.Email.Host,
+		cfg.Email.Port,
+		cfg.Email.User,
+		cfg.Email.Pass,
+	)
+
+	// validator
 	validator := validator.NewGoValidator(db)
-	return http.NewUserHandler(userService, validator)
+
+	// === adapter
+	// --- user
+	userRepo := adapterRepo.NewUserRepoPostgres(db)
+	// --- auth
+	authRepo := adapterRepo.NewUserVerificationRepo(db)
+
+	// === service
+	// --- user
+	hashService := hash.NewBcryptHash()
+	// --- auth
+	authService := service.NewAuthService(userRepo, hashService, authRepo, emailAdapter, cfg)
+
+	return &Handlers{
+		Auth: http.NewAuthHandler(authService, validator),
+	}
 }
