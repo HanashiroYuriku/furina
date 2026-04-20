2a7795a1b60d5eb18d7518070149311d80a5208b
HanashiroYuriku <devan051003@gmail.com>
2026-04-20 21:42:09 +0700

    add: add refresh token to generate token function

--- pkg/jwt/generator.go
+++ pkg/jwt/generator.go
@@ -1,6 +1,6 @@
 package jwt
 
-import  (
+import (
 	"time"
 
 	"be-ayaka/config"
@@ -11,33 +11,53 @@ import  (
 // CustomClaims defines the structure of JWT claims
 type CustomClaims struct {
 	UserID string `json:"userId"`
-	Role string `json:"role"`
+	Role   string `json:"role"`
 	jwt.RegisteredClaims
 }
 
-// GenerateToken generates a JWT token for a given user ID and role
-func GenerateToken(cfg *config.Config, userID string, role string) (string, error) {
-	expirationTime := time.Now().Add(time.Duration(cfg.JWT.Expired) * time.Hour)
+type TokenPair struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
 
-	// create claims
-	claims := &CustomClaims{
+// GenerateToken generates a JWT token for a given user ID and role
+func GenerateToken(cfg *config.Config, userID string, role string) (*TokenPair, error) {
+	// access token
+	accessExp := time.Now().Add(time.Duration(cfg.JWT.Expired) * time.Minute)
+	accessClaims := &CustomClaims{
 		UserID: userID,
-		Role: role,
+		Role:   role,
 		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			ExpiresAt: jwt.NewNumericDate(accessExp),
 			IssuedAt: jwt.NewNumericDate(time.Now()),
 			Issuer: cfg.App.Name,
 		},
 	}
 
-	// create token
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	// sign token with secret
-	tokenString, err := token.SignedString([]byte(cfg.JWT.Secret))
+	accessToken, err := jwt.NewWithClaims(jwt.SigningMethodES256, accessClaims).SignedString([]byte(cfg.JWT.Secret))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return tokenString, nil
+	// refresh token
+	refreshExp := time.Now().Add(time.Duration(cfg.JWT.RefreshExpired) * time.Minute)
+	refreshClaims := &CustomClaims{
+		UserID: userID,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(refreshExp),
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			Issuer: cfg.App.Name,
+		},
+	}
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodES256, refreshClaims).SignedString([]byte(cfg.JWT.RefreshSecret))
+	if err != nil {
+		return nil, err
+	}
+
+	return &TokenPair{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
 }
