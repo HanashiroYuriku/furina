3ecf926bffe9af45472498d259cde4a69e7030fe
HanashiroYuriku <devan051003@gmail.com>
2026-04-20 21:19:28 +0700

    add: add login service and add condition if user already verified in verify user service

--- internal/core/service/auth_service.go
+++ internal/core/service/auth_service.go
@@ -6,6 +6,8 @@ import (
 	"be-ayaka/internal/core/entity"
 	"be-ayaka/internal/core/port"
 	"be-ayaka/pkg/hash"
+	"be-ayaka/pkg/jwt"
+	"be-ayaka/pkg/logger"
 	"be-ayaka/pkg/utils"
 	"errors"
 	"fmt"
@@ -17,6 +19,7 @@ type AuthService interface {
 	Create(user *entity.UserRequest) error
 	ResendVerification(email string) error
 	VerifyUser(token string) error
+	Login(emailUsername, password, requestId string) (*entity.LoginResponse, error)
 }
 
 type authServiceImpl struct {
@@ -74,23 +77,78 @@ func (s *authServiceImpl) ResendVerification(email string) error {
 		return err
 	}
 
+	if userData.IsVerified {
+		return customerrors.ErrAccountAlreadyVerified
+	}
+
 	go s.generateAndSendVerif(userData)
 	return nil
 }
 
 func (s *authServiceImpl) VerifyUser(token string) error {
-	user, err := s.authRepo.FindByToken(token)
+	userVerif, err := s.authRepo.FindByToken(token)
 	if err != nil {
 		return err
 	}
 
-	if time.Now().After(user.ExpiredAt) {
+	if time.Now().After(userVerif.ExpiredAt) {
 		return customerrors.ErrTokenExpired
 	}
 
-	return s.userRepo.VerifUser(user.UserID)
+	user, err := s.userRepo.FindByID(userVerif.UserID)
+	if err != nil {
+		return err
+	}
+
+	if user.IsVerified {
+		return customerrors.ErrAccountAlreadyVerified
+	}
+
+	return s.userRepo.VerifUser(userVerif.UserID)
 }
 
+func (s *authServiceImpl) Login(emailUsername, password, requestId string) (*entity.LoginResponse, error) {
+	user, err := s.userRepo.FindByEmailUsername(emailUsername)
+	if err != nil {
+		return nil, err
+	}
+
+	if !user.IsVerified {
+		return nil, customerrors.ErrAccountInactive
+	}
+
+	if err := s.hashService.ComparePassword(user.Password, password); err != nil {
+		return nil, customerrors.ErrInvalidPassword
+	}
+
+	tokens, err := jwt.GenerateToken(s.config, user.ID, user.Role)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.userRepo.UpdateRefreshToken(user.ID, tokens.RefreshToken); err != nil {
+		return nil, err
+	}
+
+	go logger.Log(user.ID, "INFO", fmt.Sprintf("User %s logged in successfully", user.Username), requestId)
+
+	data := &entity.LoginResponse{
+		AccessToken: tokens.AccessToken,
+		RefreshToken: tokens.RefreshToken,
+		ExpiresIn:   int64(s.config.JWT.Expired),
+		User: entity.UserResponse{
+			ID:       user.ID,
+			Username: user.Username,
+			Email:    user.Email,
+			Role:     user.Role,
+		},
+	}
+
+	return data, nil
+}
+
+
+// internal function
 func (s *authServiceImpl) generateAndSendVerif(user *entity.User) error {
 	token := utils.GenerateID("TOKEN")
 
