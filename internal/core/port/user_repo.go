f993c147f3bc76356c015e8df08bfbfa5033f50f
HanashiroYuriku <devan051003@gmail.com>
2026-04-20 21:17:20 +0700

    add:  add function for update refresh token and change return type of find by email username function

--- internal/core/port/user_repo.go
+++ internal/core/port/user_repo.go
@@ -4,7 +4,8 @@ import "be-ayaka/internal/core/entity"
 
 type UserRepository interface {
 	FindByID(id string) (*entity.User, error)
-	FindByEmailUsername(value string) (*entity.UserResponse, error)
+	FindByEmailUsername(value string) (*entity.User, error)
 	Create(user *entity.User) error
 	VerifUser(id string) error
+	UpdateRefreshToken(id string, token string) error
 }
