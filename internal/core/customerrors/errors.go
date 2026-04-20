5ea7b5a17fb07c814027f9e0707bf9fbf89d18ce
HanashiroYuriku <devan051003@gmail.com>
2026-04-20 21:14:37 +0700

    add: add new custom errors [account inactive, invalid credentials, and account already exist]

--- internal/core/customerrors/errors.go
+++ internal/core/customerrors/errors.go
@@ -3,12 +3,15 @@ package customerrors
 import "errors"
 
 var (
-	ErrDataNotFound     = errors.New("Data Not Found")
-	ErrInvalidPassword  = errors.New("Invalid Password")
-	ErrTokenExpired     = errors.New("Token Expired")
-	ErrDataConflict     = errors.New("Data Conflict")
-	ErrBadRequest       = errors.New("Bad Request")
-	ErrColldownActive   = errors.New("Please Wait 5 Minutes Before Sending Again")
+	ErrDataNotFound           = errors.New("Data Not Found")
+	ErrInvalidPassword        = errors.New("Invalid Password")
+	ErrTokenExpired           = errors.New("Token Expired")
+	ErrDataConflict           = errors.New("Data Conflict")
+	ErrBadRequest             = errors.New("Bad Request")
+	ErrColldownActive         = errors.New("Please Wait 5 Minutes Before Sending Again")
+	ErrInvalidCredentials     = errors.New("Invalid Username / Email")
+	ErrAccountInactive        = errors.New("Account Not Verified")
+	ErrAccountAlreadyVerified = errors.New("Account Already Verified")
 )
 
 type ValidationError struct {
