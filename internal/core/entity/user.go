569170135a5774240272d40ab9c2f2c27d20b4cb
HanashiroYuriku <devan051003@gmail.com>
2026-04-20 21:16:21 +0700

    add: add refresh token to user struct, add omit ompty to json is verify at user response struct, add struct for login response and request

--- internal/core/entity/user.go
+++ internal/core/entity/user.go
@@ -8,15 +8,16 @@ import (
 
 // User represents a user in the system.
 type User struct {
-	ID         string `gorm:"primaryKey;type:varchar(50)"`
-	Username   string `gorm:"unique;not null;type:varchar(20)"`
-	Email      string `gorm:"unique;not null;type:varchar(100)"`
-	Password   string `gorm:"not null;type:varchar(255)"`
-	Role       string `gorm:"type:varchar(20);default:'user';check:role IN ('admin', 'user')"`
-	IsVerified bool   `gorm:"default:false"`
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	ID           string  `gorm:"primaryKey;type:varchar(50)"`
+	Username     string  `gorm:"unique;not null;type:varchar(20)"`
+	Email        string  `gorm:"unique;not null;type:varchar(100)"`
+	Password     string  `gorm:"not null;type:varchar(255)"`
+	Role         string  `gorm:"type:varchar(20);default:'user';check:role IN ('admin', 'user')"`
+	IsVerified   bool    `gorm:"default:false"`
+	RefreshToken *string `gorm:"type:text"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeletedAt    gorm.DeletedAt `gorm:"index"`
 }
 
 // user struct for api response
@@ -25,7 +26,7 @@ type UserResponse struct {
 	Username   string `json:"username"`
 	Email      string `json:"email"`
 	Role       string `json:"role"`
-	IsVerified bool   `json:"isVerified"`
+	IsVerified bool   `json:"isVerified,omitempty"`
 }
 
 // user struct for api request
@@ -33,4 +34,16 @@ type UserRequest struct {
 	Username string `json:"username" validate:"required,min=3,max=20,unique=user->username,whitespace,username"`
 	Email    string `json:"email" validate:"required,max=100,email,unique=user->email,whitespace"`
 	Password string `json:"password" validate:"required,complexpassword"`
+}
+
+type LoginResponse struct {
+	AccessToken  string       `json:"accessToken"`
+	RefreshToken string       `json:"refreshToken"`
+	ExpiresIn    int64        `json:"expiresIn"`
+	User         UserResponse `json:"user"`
+}
+
+type LoginRequest struct {
+	EmailUsername string `json:"emailUsername" validate:"required"`
+	Password      string `json:"password" validate:"required"`
 }
