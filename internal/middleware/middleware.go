0286e39b55c8f0f084400be7b2cf4029dcced74d
HanashiroYuriku <devan051003@gmail.com>
2026-04-30 13:58:12 +0700

    refract: change GetRequestID import from utils to request_id and add logging before return a response

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -5,8 +5,9 @@ import (
 
 	"be-ayaka/config"
 	customjwt "be-ayaka/pkg/jwt"
+	"be-ayaka/pkg/logger"
+	"be-ayaka/pkg/request_id"
 	"be-ayaka/pkg/response"
-	"be-ayaka/pkg/utils"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
@@ -14,11 +15,12 @@ import (
 
 func RequireAuth(cfg *config.Config) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		requestId := utils.GetRequestID(c)
+		requestId := requestid.GetRequestID(c)
 
 		authHeader := c.Get("Authorization")
 
 		if authHeader == "" {
+			logger.Log("AUTH", "WARN", "Authorization header is missing", requestId)
 			return c.Status(fiber.StatusUnauthorized).JSON(response.NewErrorResponse(
 				response.Unauthorized,
 				"Authorization header is missing",
@@ -28,6 +30,7 @@ func RequireAuth(cfg *config.Config) fiber.Handler {
 
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
+			logger.Log("AUTH", "WARN", "Invalid Token Format", requestId)
 			return c.Status(fiber.StatusUnauthorized).JSON(response.NewErrorResponse(
 				response.Unauthorized,
 				"Invalid Token Format",
@@ -39,12 +42,14 @@ func RequireAuth(cfg *config.Config) fiber.Handler {
 		claims := &customjwt.CustomClaims{}
 		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				logger.Log("AUTH", "WARN", "Unexpected signing method", requestId)
 				return nil, fiber.ErrUnauthorized
 			}
 			return []byte(cfg.JWT.Secret), nil
 		})
 
 		if err != nil || !token.Valid {
+			logger.Log("AUTH", "WARN", "Token Invalid or Expired", requestId)
 			return c.Status(fiber.StatusUnauthorized).JSON(response.NewErrorResponse(
 				response.Unauthorized,
 				"Token Invalid or Expired",
