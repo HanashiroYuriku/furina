2a8a0cf1dcee0d5d81b742931cf3a823432ecaa1
HanashiroYuriku <devan051003@gmail.com>
2026-04-30 14:00:05 +0700

    refract: change GetRequestID import from utils to request_id; add: add swagger

--- internal/delivery/http/health.go
+++ internal/delivery/http/health.go
@@ -3,7 +3,7 @@ package http
 import (
 	"be-ayaka/config"
 	ayaka "be-ayaka/pkg/logger"
-	"be-ayaka/pkg/utils"
+	"be-ayaka/pkg/request_id"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,26 +12,33 @@ import (
 
 type HealthCheckHandler struct {
 	Config *config.Config
-	db *gorm.DB
+	db     *gorm.DB
 }
 
 func NewHealthCheckHandler(cfg *config.Config, db *gorm.DB) *HealthCheckHandler {
 	return &HealthCheckHandler{
 		Config: cfg,
-		db: db,
+		db:     db,
 	}
 }
 
+// Check server and database health status
+// @Summary Health Check
+// @Description make sure the server is running and database connection
+// @Tags System
+// @Produce json
+// @Success 200 {object} response.Response
+// @Router /health [get]
 func (h *HealthCheckHandler) Check(c *fiber.Ctx) error {
-	requestId := utils.GetRequestID(c)
+	requestId := requestid.GetRequestID(c)
 	go ayaka.Log("SYSTEM", "INFO", "Ayaka Server is running", requestId)
-	
+
 	appStatus := "UP"
 	dbStatus := "UP"
 	httpCode := fiber.StatusOK
 
-	sqlDB, err := h.db.DB() 
-	
+	sqlDB, err := h.db.DB()
+
 	if err != nil || sqlDB.Ping() != nil {
 		appStatus = "DOWN"
 		dbStatus = "DOWN"
@@ -39,12 +46,12 @@ func (h *HealthCheckHandler) Check(c *fiber.Ctx) error {
 	}
 
 	return c.Status(httpCode).JSON(fiber.Map{
-		"name":    h.Config.App.Name,
-		"status":  appStatus,
-		"time":    time.Now().Format(time.RFC3339),
+		"name":   h.Config.App.Name,
+		"status": appStatus,
+		"time":   time.Now().Format(time.RFC3339),
 		"components": fiber.Map{
-			"server":   "UP",       
-			"database": dbStatus,   
+			"server":   "UP",
+			"database": dbStatus,
 		},
 	})
 }
