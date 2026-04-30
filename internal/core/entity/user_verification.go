b9d2d30a3052bf423b9f4d2e787737fffb5fc593
HanashiroYuriku <devan051003@gmail.com>
2026-04-30 14:10:42 +0700

    fix: add uniqueIndex to UserID

--- internal/core/entity/user_verification.go
+++ internal/core/entity/user_verification.go
@@ -4,7 +4,7 @@ import "time"
 
 type UserVerification struct {
 	ID        string    `gorm:"primaryKey;type:varchar(50)"`
-	UserID    string    `gorm:"index;not null;type:varchar(50)"`
+	UserID    string    `gorm:"uniqueIndex;not null;type:varchar(50)"`
 	Email     string    `gorm:"unique;not null;type:varchar(255)"`
 	Token     string    `gorm:"unique;not null;type:varchar(255)"`
 	ExpiredAt time.Time `gorm:"not null"`
@@ -12,5 +12,5 @@ type UserVerification struct {
 }
 
 type UserVerificationRequest struct {
-	Email string `json:"email" validate:"required,incolumn=user_verification->email"`
+	Email string `json:"email" validate:"required,incolumn=user_verifications->email"`
 }
