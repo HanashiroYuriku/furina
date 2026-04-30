e21f86ff4ae40f12d5cfe7fe5855f51a1f4385d2
HanashiroYuriku <devan051003@gmail.com>
2026-04-30 14:03:53 +0700

    add: add unauthorized error and add conflict error and not found error with constractors

--- internal/core/customerrors/errors.go
+++ internal/core/customerrors/errors.go
@@ -6,7 +6,7 @@ var (
 	ErrDataNotFound           = errors.New("Data Not Found")
 	ErrInvalidPassword        = errors.New("Invalid Password")
 	ErrTokenExpired           = errors.New("Token Expired")
-	ErrDataConflict           = errors.New("Data Conflict")
+	ErrUnauthorized           = errors.New("Unauthorized")
 	ErrBadRequest             = errors.New("Bad Request")
 	ErrColldownActive         = errors.New("Please Wait 5 Minutes Before Sending Again")
 	ErrInvalidCredentials     = errors.New("Invalid Username / Email")
@@ -24,6 +24,36 @@ func (e *ValidationError) Error() string {
 
 func NewValidationError(detail string) error {
 	return &ValidationError{
+		Detail: detail,
+	}
+}
+
+
+type ConflictError struct {
+	Detail string
+}
+
+func (e *ConflictError) Error() string {
+	return "Data Conflict"
+}
+
+func NewConflictError(detail string) error {
+	return &ConflictError{
+		Detail: detail,
+	}
+}
+
+
+type NotFoundError struct {
+	Detail string
+}
+
+func (e *NotFoundError) Error() string {
+	return "Data Not Found"
+}
+
+func NewNotFoundError(detail string) error {
+	return &NotFoundError{
 		Detail: detail,
 	}
 }
