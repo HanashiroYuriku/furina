85a15cf8b926dbb800cf3b9534dba393f6d0ad30
HanashiroYuriku <devan051003@gmail.com>
2026-04-30 14:11:21 +0700

    fix: add context; add: add function for find user by refresh token

--- internal/core/port/user_repo.go
+++ internal/core/port/user_repo.go
@@ -1,11 +1,15 @@
 package port
 
-import "be-ayaka/internal/core/entity"
+import (
+	"be-ayaka/internal/core/entity"
+	"context"
+)
 
 type UserRepository interface {
-	FindByID(id string) (*entity.User, error)
-	FindByEmailUsername(value string) (*entity.User, error)
-	Create(user *entity.User) error
-	VerifUser(id string) error
-	UpdateRefreshToken(id string, token string) error
+	FindByID(ctx context.Context, id string) (*entity.User, error)
+	FindByEmailUsername(ctx context.Context, value string) (*entity.User, error)
+	Create(ctx context.Context, user *entity.User) error
+	VerifUser(ctx context.Context, id string) error
+	UpdateRefreshToken(ctx context.Context, id string, token string) error
+	FindByRefreshToken(ctx context.Context, token string) (*entity.User, error)
 }
