73aaee2242ebd82e2f25012ea0d6300b695006f3
HanashiroYuriku <devan051003@gmail.com>
2026-04-30 13:45:01 +0700

    add: add swagger

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ import (
 	"be-ayaka/cmd"
 )
 
+// @title Furina (Material Tracer) API
+// @version 1.0
+// @description API Documentation for Material Tracer - FURINA.
+// @contact.name Hanashiro Yuriku
+// @host localhost:8000
+// @BasePath /
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer" followed by a space and JWT token.
 func main() {
 	if err := cmd.Execute(); err != nil {
 		log.Fatalf("Fail run application: %v", err)
