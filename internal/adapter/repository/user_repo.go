d0879d786a3f491747244b2b84790a922b4ecb54
HanashiroYuriku <devan051003@gmail.com>
2026-04-30 14:08:19 +0700

    refract: implement transaction db; add: add new function for find user by refresh token

--- internal/adapter/repository/user_repo.go
+++ internal/adapter/repository/user_repo.go
@@ -4,24 +4,25 @@ import (
 	"be-ayaka/internal/core/customerrors"
 	"be-ayaka/internal/core/entity"
 	"be-ayaka/internal/core/port"
+	"context"
 	"errors"
 
 	"gorm.io/gorm"
 )
 
-type userRepoPostgres struct {
+type userRepo struct {
 	db *gorm.DB
 }
 
-func NewUserRepoPostgres(db *gorm.DB) port.UserRepository {
-	return &userRepoPostgres{
+func NewUserRepo(db *gorm.DB) port.UserRepository {
+	return &userRepo{
 		db: db,
 	}
 }
 
-func (r *userRepoPostgres) FindByID(id string) (*entity.User, error) {
+func (r *userRepo) FindByID(ctx context.Context, id string) (*entity.User, error) {
 	var user entity.User
-	err := r.db.First(&user, "id = ?", id).Error
+	err := r.db.WithContext(ctx).First(&user, "id = ?", id).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, customerrors.ErrDataNotFound
@@ -31,9 +32,9 @@ func (r *userRepoPostgres) FindByID(id string) (*entity.User, error) {
 	return &user, nil
 }
 
-func (r *userRepoPostgres) FindByEmailUsername(value string) (*entity.User, error) {
+func (r *userRepo) FindByEmailUsername(ctx context.Context, value string) (*entity.User, error) {
 	var user entity.User
-	err := r.db.Where("email = ? OR username = ?", value, value).First(&user).Error
+	err := r.db.WithContext(ctx).Where("email = ? OR username = ?", value, value).First(&user).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, customerrors.ErrInvalidCredentials
@@ -43,14 +44,27 @@ func (r *userRepoPostgres) FindByEmailUsername(value string) (*entity.User, erro
 	return &user, nil
 }
 
-func (r *userRepoPostgres) Create(user *entity.User) error {
-	return r.db.Create(user).Error
+func (r *userRepo) Create(ctx context.Context, user *entity.User) error {
+	db := ExtractTx(ctx, r.db)
+	return db.WithContext(ctx).Create(user).Error
 }
 
-func (r *userRepoPostgres) VerifUser(id string) error {
-	return r.db.Model(&entity.User{}).Where("id = ?", id).Update("is_verified", true).Error
+func (r *userRepo) VerifUser(ctx context.Context, id string) error {
+	return r.db.WithContext(ctx).Model(&entity.User{}).Where("id = ?", id).Update("is_verified", true).Error
 }
 
-func (r *userRepoPostgres) UpdateRefreshToken(id string, token string) error {
-	return r.db.Model(&entity.User{}).Where("id = ?", id).Update("refresh_token", token).Error
+func (r *userRepo) UpdateRefreshToken(ctx context.Context, id string, token string) error {
+	return r.db.WithContext(ctx).Model(&entity.User{}).Where("id = ?", id).Update("refresh_token", token).Error
+}
+
+func (r *userRepo) FindByRefreshToken(ctx context.Context, token string) (*entity.User, error) {
+	var user entity.User
+	err := r.db.WithContext(ctx).Where("refresh_token = ?", token).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, customerrors.ErrInvalidCredentials
+		}
+		return nil, err
+	}
+	return &user, nil
 }
