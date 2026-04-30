ad2ecfa5ea400ebe271c1bba991ba8700898034f
HanashiroYuriku <devan051003@gmail.com>
2026-04-30 13:53:26 +0700

    add: implement transaction manager for GORM

---
+++ internal/adapter/repository/tx_manager.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"context"
+
+	"be-ayaka/internal/core/port"
+	"gorm.io/gorm"
+)
+
+type txKey struct{}
+
+type gormTxManager struct {
+	db *gorm.DB
+}
+
+func NewTxManager(db *gorm.DB) port.TxManager {
+	return &gormTxManager{db: db}
+}
+
+func (tm *gormTxManager) WithTx(ctx context.Context, fn func(ctx context.Context) error) error {
+	return tm.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		txCtx := context.WithValue(ctx, txKey{}, tx)
+		return fn(txCtx)		
+	})
+}
+
+func ExtractTx(ctx context.Context, defaultDB *gorm.DB) *gorm.DB {
+	if tx, ok := ctx.Value(txKey{}).(*gorm.DB); ok {
+		return tx
+	}
+	return defaultDB
+}
