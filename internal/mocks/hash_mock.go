7f1662ddf01fa29071c84d3031906576eec956b6
HanashiroYuriku <devan051003@gmail.com>
2026-05-01 18:10:08 +0700

    feat: add mock for hash service

---
+++ internal/mocks/hash_mock.go
@@ -0,0 +1,20 @@
+package mocks
+
+import (
+	"github.com/stretchr/testify/mock"
+)
+
+// mock hash service 
+type MockHashService struct {
+	mock.Mock
+}
+
+func (m *MockHashService) HashPassword(password string) (string, error) {
+	args := m.Called(password)
+	return args.String(0), args.Error(1)
+}
+
+func (m *MockHashService) ComparePassword(password, hash string) (error) {
+	args := m.Called(password, hash)
+	return args.Error(0)
+}
