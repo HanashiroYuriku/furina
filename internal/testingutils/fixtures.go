6c8c23855fb76603205cbbd71fc4a43a9e133e49
HanashiroYuriku <devan051003@gmail.com>
2026-05-17 19:56:03 +0700

    feat: add make json request for testing utils

--- internal/testingutils/fixtures.go
+++ internal/testingutils/fixtures.go
@@ -1,6 +1,13 @@
 package testingutils
 
-import "be-ayaka/config"
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"be-ayaka/config"
+)
 
 func GetDummyConfig() *config.Config {
 	return &config.Config{
@@ -12,4 +19,22 @@ func GetDummyConfig() *config.Config {
 
 func StringPtr(s string) *string {
     return &s
+}
+
+func MakeJSONRequest(method, path string, body interface{}) *http.Request {
+	var bodyReader io.Reader
+	if body != nil {
+		if b, ok := body.([]byte); ok {
+			bodyReader = bytes.NewBuffer(b)
+		} else {
+			marshaled, _ := json.Marshal(body)
+			bodyReader = bytes.NewBuffer(marshaled)
+		}
+	}
+
+	req, _ := http.NewRequest(method, path, bodyReader)
+	req.Host = "localhost"
+	req.Header.Set("Content-Type", "application/json")
+
+	return req
 }
